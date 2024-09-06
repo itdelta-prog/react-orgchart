390f45b3eafefb22a06c126c607d6d9b371688a1
itdelta-prog <delta@it-delta.ru>
2024-09-06 14:51:29 +0300

    undefined check

--- src/components/ChartNode.js
+++ src/components/ChartNode.js
@@ -40,6 +40,9 @@ const ChartNode = ({
   const getExpandedLocalStorage = (name, returnFalse) => {
     const value = localStorage.getItem(window.location.pathname + ' ' + datasource.id + '_' + name);
     if(value) {
+      if(value === 'undefined') {
+        return undefined
+      }
       return 'true' === value
     }
     return returnFalse ? false : undefined
